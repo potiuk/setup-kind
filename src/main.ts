7345a7e015d9844bab5f4a8a8cfb4eb3cf7f91ef
Jarek Potiuk <jarek@potiuk.com>
2020-10-11 18:57:00 +0200

    Replaces deprecated add-path with appending to GITHUB_PATH.
    Fixes #28
    
    Fixes deprecation warning about "add-path".
    
    Add Path was deprecated in
    https://github.blog/changelog/2020-10-01-github-actions-deprecating-set-env-and-add-path-commands/
    and will be removed soon, so this PR changes the mechanism to
    the new one.
    
    The path is added to the ${GITHUB_PATH} file instead as described in
    https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-commands-for-github-actions#adding-a-system-path
    
    It also adds lib folder to the repository, so that we can use
    the action using commit hashes which is the recommended way of
    using actions. See:
    
    https://docs.github.com/en/free-pro-team@latest/actions/learn-github-actions/security-hardening-for-github-actions#using-third-party-actions

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import * as core from '@actions/core';
 import { KindConfig, getKindConfig, getKind } from './kind';
+const fs = require('fs');
 
+fs.appendFileSync('message.txt', 'data to append');
 async function run() {
   try {
     let cfg: KindConfig = getKindConfig();
     let toolPath: string = await getKind(cfg.version);
-    core.addPath(toolPath);
-    await cfg.createCluster();
+    const pathFile: string = process.env[`GITHUB_PATH`] || '';
+    if (pathFile == '') {
+      core.setFailed("The GITHUB_PATH environment variable is not set!")
+    } else {
+      fs.appendFileSync(pathFile, toolPath + "\n");
+      await cfg.createCluster();
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
